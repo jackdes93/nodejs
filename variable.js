52ae2886d4698edccb709ca03ac2464b50b00758
Phuong Binh <phuong.binh0310@gmail.com>
2018-03-29 21:58:36 +0700

    Second
    Update something

--- variable.js
+++ variable.js
@@ -1,2 +1,22 @@
-var a = 10;
-console.log(a);
+// const http = require('http');
+//
+// const hostname = '127.0.0.1';
+// const port = 3000;
+//
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World!');
+// });
+//
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+var diem = 10;
+
+if (diem >= 5) {
+  console.log('Success!');
+} else {
+  console.log('Fail! :(');
+}
