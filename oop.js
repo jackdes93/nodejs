8f50c11f5221b73919281b30b557d20b9b78e832
Phuong Binh <phuong.binh0310@gmail.com>
2018-03-29 23:52:39 +0700

    News

--- oop.js
+++ oop.js
@@ -7,3 +7,16 @@
 // }
 //
 // person.showinfo();
+/* ClassName.prototype.methodName = function () {};*/
+
+function KhoaHoc(ten, hocPhi) {
+  this.Ten = ten;
+  this.HocPhi = hocPhi;
+}
+
+KhoaHoc.prototype.description = function () {
+  console.log("Hello " + this.Ten + " " + this.HocPhi);
+};
+
+var student = new KhoaHoc("Lap Trinh Node JS", 12000000);
+student.description();
