384e7a7642a0594072eb000b9a53db078418c916
Phuong Binh <phuong.binh0310@gmail.com>
2018-03-29 23:30:21 +0700

    Four

---
+++ oop.js
@@ -0,0 +1,9 @@
+// var person = {
+//   firstname  : "Dai",
+//   lastname   : "Binh",
+//   showinfo  : function() {
+//     console.log("Hello " + this.firstname + " " + this.lastname);
+//   }
+// }
+//
+// person.showinfo();
