fb6404138152a7aeaa2a8766870cdc490f5a9fee
Jackdes <phuong.binh0310@gmail.com>
2018-04-14 00:19:18 +0700

    End First Day

--- Web-Chat-Real-Time/index.js
+++ Web-Chat-Real-Time/index.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var app = express();
+
 app.use(express.static("./public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 var server = require("http").Server(app);
+var io = require("socket.io")(server);
 server.listen(8000);
 
+io.on("connection", function(socket){
+  console.log("Have connection with id: "+ socket.id);
+    socket.on("disconnect", function(){
+      console.log(socket.id + " disconnect");
+    });
+});
+
+// Route
 app.get("/", function(req, res){
   res.render("homepage");
 });
