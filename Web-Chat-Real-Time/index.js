24157e252e6f84ac3f0e55cda95f2879828d1e4f
Jackdes <phuong.binh0310@gmail.com>
2018-05-07 22:16:30 +0700

    Update
    update, improve code

--- Web-Chat-Real-Time/index.js
+++ Web-Chat-Real-Time/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
-var arrayDataUser = ['AAA','dev_pbinh'];
+var arrayDataUser = [];
+var testArray = ['hades']
 var message = {};
 var constants = require('./public/constvariable');
 //  Set some propertise
@@ -18,17 +19,27 @@ io.on("connection", function(socket){
       console.log(socket.id + " disconnect");
     });
     socket.on(constants.CLIENT_REGISTER_USER, function(data){
-      if(arrayDataUser.indexOf(data['username']) >= 0){
-        message = {'message':data['username'] + ' đã tồn tại trong hệ thống', 'status' : 1};
-        socket.emit(constants.REQUEST_NOTIFI_T_CLIENT,message);
-      } else {
-        message = {'message':'Bạn đã đăng ký thành công với username là '+ data['username'], 'status' : 0};
-        socket.emit(constants.REQUEST_NOTIFI_T_CLIENT, message);
-      }
-
+       if (arrayDataUser.length == 0) {
+         arrayDataUser.push({'username': data['username'], 'password':data['password']});
+       } else {
+         checkUserIsHave(arrayDataUser, data)
+       }
     });
 });
 
+/*
+  Some function for server
+*/
+function findIndexByKey(arrayDataUser, key, valueSearch) { for (var i = 0; i < arrayDataUser.length; i++) { if(arrayDataUser[i][key] == valueSearch) { return i; } } return null; }
+function checkUserIsHave(arrayDataUser, data) {
+  if(findIndexByKey(arrayDataUser, 'username', data['username']) >= 0){
+    message = {'message':data['username'] + ' đã tồn tại trong hệ thống', 'status' : 1};
+  } else {
+    message = {'message':'Bạn đã đăng ký thành công với username là '+ data['username'], 'status' : 0};
+    arrayDataUser.push({'username': data['username'], 'password':data['password']});
+  }
+  socket.emit(constants.REQUEST_NOTIFI_T_CLIENT, message);
+}
 // Route
 app.get("/", function(req, res){
   res.render("homepage");
